c6c33b38334a26c50b3a4d48f07dbbf02659f1b0
Sal Frisina <sfrisina@gmail.com>
2025-12-06 19:20:45 -0500

    added more stuffs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component,EventEmitter, Input, Output, computed} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -11,15 +11,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
- selectedUser = signal(DUMMY_USERS[randomIndex])
- imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  @Input({ required: true }) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter()
+  // avatar = input.required<string>()
+  // name = input.required<string>()
 
-//  get imagePath(){
-//   return 'assets/users/' + this.selectedUser.avatar
-//  }
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar
+  })
+
 
  onSelectUser(){
-  const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-  this.selectedUser.set(DUMMY_USERS[randomIndex])
+  this.select.emit()
  }
 }
